97714593f1373d52d4bc71954de4c3e36d9752cd
Chelsey Childs <ch764721@dal.ca>
2017-06-28 18:46:09 -0300

    Combined all the features together
    Made a new UI
    Allows users to specify frequency and email addresses
    Alert Mode toggles if we should send the email or not
    
    TODO: Need to be able to stop everything

--- Ubicomp_Group_7/app/src/main/java/com/example/aleysha/ubicomp_group_7/CameraView.java
+++ Ubicomp_Group_7/app/src/main/java/com/example/aleysha/ubicomp_group_7/CameraView.java
@@ -2,6 +2,7 @@ package com.example.aleysha.ubicomp_group_7;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.os.CountDownTimer;
 import android.os.Environment;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -9,9 +10,12 @@ import android.view.SurfaceView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimerTask;
+import java.util.Timer;
 
 ///
 /// The following code was taken from:
@@ -44,25 +48,41 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
-        if (mHolder.getSurface() == null){ //check if the surface is ready to receive camera data
-            return;
+
+        try{
+            mCamera.setPreviewDisplay(mHolder);
+        }
+        catch(Exception e){}
+
+        class PhotoTimer extends TimerTask
+        {
+            public void run()
+            {
+                try {
+                    if (mHolder.getSurface() == null){ //check if the surface is ready to receive camera data
+                        return;
+                    }
+
+                    try {
+                        mCamera.stopPreview();
+                    } catch (Exception e) {}
+
+                    mCamera.startPreview();
+
+                    safeToTakePicture = true;
+                    if (safeToTakePicture) {
+                        mCamera.takePicture(null, null, mPicture);
+                        safeToTakePicture = false;
+                    }
+
+
+                } catch (Exception e) {}
+            }
         }
 
-        try {
-            mCamera.stopPreview();
-        } catch (Exception e) {}
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new PhotoTimer(), 0, 60000*Integer.parseInt(MainActivity.cameraFreq.getText().toString()));
 
-        try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
-
-            safeToTakePicture = true;
-            if (safeToTakePicture) {
-                mCamera.takePicture(null, null, mPicture);
-                safeToTakePicture = false;
-            }
-
-        } catch (IOException e) {}
     }
 
     @Override
@@ -105,8 +125,42 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
         File mediaFile;
         mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
 
+        MainActivity.UnsentImageAddresses.add(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+
+        File documentStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "GPSCoordindates");
+
+        if(!documentStorageDir.exists())
+        {
+            if(!documentStorageDir.mkdirs())
+            {
+                return null;
+            }
+        }
+        try {
+            File gpsFile = new File(documentStorageDir, "GPS_" + timeStamp + ".txt");
+            String gpsContent = "";
+            for (int i = 0; i < MainActivity.UnsavedGPSLinks.size(); i++) {
+                gpsContent += MainActivity.UnsavedGPSLinks.get(i) + "\n";
+            }
+
+            FileWriter filewriter = new FileWriter(gpsFile);
+            filewriter.append(gpsContent);
+            filewriter.flush();
+            filewriter.close();
+
+            MainActivity.UnsavedGPSLinks.clear();
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        if(MainActivity.alertModeCheckBox.isChecked())
+            MainActivity.sendGmail();
+
         return mediaFile;
     }
 }
+
 
 
--- Ubicomp_Group_7/app/src/main/java/com/example/aleysha/ubicomp_group_7/MainActivity.java
+++ Ubicomp_Group_7/app/src/main/java/com/example/aleysha/ubicomp_group_7/MainActivity.java
@@ -16,10 +16,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.hardware.Camera;
 import android.widget.FrameLayout;
 import android.widget.Toast;
+import java.util.*;
 
 /// This activity is the base of the application. It is the only activity for the application
 /// as the user will not be able to navigate the application due to the application running
@@ -27,13 +30,21 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     //widgets in the UI
-    private Button button, button2, sendButton,camerabutton;
-    private TextView textView;
+    public static Button start;
+    public static EditText gpsFreq, cameraFreq, deviceEmail, devicePassword, parentEmail;
+    public static CheckBox alertModeCheckBox;
+    public static TextView gpsView;
     private LocationManager locationManager;
     private LocationListener locationListener;
     private Camera camera = null;
     private CameraView cameraView = null;
     private int cameraId;
+    public static Boolean alertMode = true;
+
+    // Google map links of GPS coords
+    public static ArrayList<String> UnsavedGPSLinks = new ArrayList<String>();
+    public static ArrayList<String> UnsentImageAddresses = new ArrayList<String>();
+    public static ArrayList<String> UnsentGPSLinks = new ArrayList<String>();
 
     static String loc ="";
     @SuppressLint("ServiceCast")
@@ -44,38 +55,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // obtain UI elements here
-        button = (Button) findViewById(R.id.button);
-        button2 = (Button) findViewById(R.id.button2);
-        camerabutton = (Button) findViewById(R.id.camerabutton);
-        textView = (TextView) findViewById(R.id.textView);
-
-        sendButton = (Button) findViewById(R.id.button3);
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                new Thread(new Runnable() {
-                    public void run() {
-                        try {
-                            GMailSender sender = new GMailSender(
-                                    "ubicomp.duck@gmail.com",
-                                    "duck@123");
-                            /*
-                            TODO: actual image path to be added
-                             */
-                            sender.addAttachment(Environment.getExternalStorageDirectory().getPath()+"/627742_workout.jpg");
-                            Log.v("MainActivity",Environment.getExternalStorageDirectory().toString());
-
-                            sender.sendMail("Coordinates + Images", "Email Coordinates are: " +loc,
-                                    "ubicomp.duck@gmail.com",
-                                    "ubicomp.parent@gmail.com");
-                        } catch (Exception e) {
-                            Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
-                        }
-                    }
-                }).start();
-            }
-        });
+        start = (Button) findViewById(R.id.StartBtn);
+        gpsFreq = (EditText) findViewById(R.id.GPSFreqEntry);
+        cameraFreq = (EditText) findViewById(R.id.CameraFreqEntry);
+        deviceEmail = (EditText) findViewById(R.id.DeviceEmailEntry);
+        devicePassword = (EditText) findViewById(R.id.DevicePasswordEntry);
+        parentEmail = (EditText) findViewById(R.id.ParentEmailEntry);
+        gpsView = (TextView) findViewById(R.id.GPSView);
+        alertModeCheckBox = (CheckBox) findViewById(R.id.AlertModeCheckBox);
 
         //location manager and listener to get location from Android
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
@@ -86,11 +73,13 @@ public class MainActivity extends AppCompatActivity {
                 String lat = "" +location.getLatitude();
                 String lon = "" +location.getLongitude();
 
-                textView.append("\n " + lat + ", " + lon);
+                gpsView.append("\n " + lat + ", " + lon);
                 /*
                 TODO: Add location coordinates
                  */
                 loc = loc + "\n 1" + lat + ", " + lon;
+                UnsavedGPSLinks.add("http://maps.google.com/maps?q="+ lat + "," + lon);
+                UnsentGPSLinks.add("http://maps.google.com/maps?q="+ lat + "," + lon);
             }
 
             @Override
@@ -126,8 +115,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
                 //set up the buttons that need this access
-                configureButton();
-                configureCamera();
+                configureStart();
         }
 
 
@@ -139,41 +127,42 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode){
             case 10:
                 if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    configureButton();
-                    configureCamera();
+                    configureStart();
                 }
         }
     }
 
-    //start location listener
-    public void configureButton(){
-        button.setOnClickListener(new View.OnClickListener(){
+    public void configureStart(){
+        start.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view){
-                //every 5 seconds take the location coordinates
-                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            public void onClick(View view)
+            {
+                configureGPS();
+                configureCamera();
             }
         });
+    }
+
+    //start location listener
+    public void configureGPS(){
+        //every 5 seconds take the location coordinates
+        locationManager.requestLocationUpdates("gps",
+                (long)(60000*Double.parseDouble(gpsFreq.getText().toString()))
+                , 0, locationListener);
     }
 
     // configure the camerabutton to open the camera upon click
     // if camera is available, will add the view to the framelayout camera_view
     public void configureCamera(){
-        camerabutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                try{
-                    camera = Camera.open(findBackFacingCamera());
-                } catch (Exception e){}
+        try{
+            camera = Camera.open(findBackFacingCamera());
+        } catch (Exception e){}
 
-                if(camera != null) {
-                    cameraView = new CameraView(view.getContext(), camera);
-                    FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
-                    camera_view.addView(cameraView);
-                }
-            }
-        });
+        if(camera != null) {
+            cameraView = new CameraView(this.getApplicationContext(), camera);
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.CameraView);
+            camera_view.addView(cameraView);
+        }
     }
 
     //stop location listener
@@ -195,4 +184,41 @@ public class MainActivity extends AppCompatActivity {
         }
         return cameraId;
     }
+
+    public static void sendGmail()
+    {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    GMailSender sender = new GMailSender(
+                            deviceEmail.getText().toString(),
+                            devicePassword.getText().toString());
+
+                    for(int i = 0; i < UnsentImageAddresses.size(); i++)
+                    {
+                        sender.addAttachment(UnsentImageAddresses.get(i));
+                    }
+
+                    String bodyCoordinates = "";
+                    for(int i = 0; i < UnsentGPSLinks.size(); i++)
+                    {
+                        bodyCoordinates += UnsentGPSLinks.get(i) + "\n";
+                    }
+
+                    Log.v("MainActivity",Environment.getExternalStorageDirectory().toString());
+
+                    sender.sendMail("STUFFED NOTIFICATION: " + new Date().toString(), bodyCoordinates,
+                            deviceEmail.getText().toString(),
+                            parentEmail.getText().toString());
+
+                    UnsentGPSLinks.clear();
+                    UnsentImageAddresses.clear();
+
+                } catch (Exception e) {
+                    //Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
+                }
+            }
+        }).start();
+    }
 }
+
